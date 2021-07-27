ebb79849d2deca8935c68ac61225a8f02be9eea4
Kingliyue <602100452@qq.com>
2021-07-28 00:17:07 +0800

    Windows 提交

--- service/service-edu/src/main/java/com/atliyue/edu/controller/VideoController.java
+++ service/service-edu/src/main/java/com/atliyue/edu/controller/VideoController.java
@@ -1,10 +1,11 @@
 package com.atliyue.edu.controller;
 
 
+import com.atliyue.edu.entity.Video;
+import com.atliyue.edu.service.VideoService;
 import com.liyue.result.Result;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,10 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/edu/video")
+@CrossOrigin
 public class VideoController {
-    public Result getVideo(){
+    @Autowired
+    private VideoService videoService ;
+    @GetMapping("getVideo/{videoId}")
+    public Result getVideo(@PathVariable String videoId){
+        Video video = videoService.getById(videoId);
+        return Result.ok().data("video",video);
+    }
+    @PutMapping("updateVideo")
+    public Result updateVideo(Video video){
+        videoService.updateById(video);
+        return  Result.ok();
+    }
+    @PostMapping("saveVideo")
+    public Result saveVideo(Video video){
+        videoService.save(video);
         return Result.ok();
     }
-
+    @DeleteMapping("deleteVideo/{videoId}")
+    public Result deleteVideo(@PathVariable String videoId){
+        videoService.removeById(videoId);
+        return Result.ok();
+    }
 }
 
