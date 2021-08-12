afbe2ef7dcebd27ec6b0ea1d1d077df8a51566cf
Kingliyue <602100452@qq.com>
2021-08-13 00:37:35 +0800

    Windows 提交

--- service/service-edu/src/main/java/com/atliyue/ApplicationStart.java
+++ service/service-edu/src/main/java/com/atliyue/ApplicationStart.java
@@ -2,12 +2,14 @@ package com.atliyue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.atliyue")
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class);
