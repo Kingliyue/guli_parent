5d3374a396bf2add7c6b3696cc38514fe80fbd2e
Kingliyue <602100452@qq.com>
2021-08-30 21:45:32 +0800

    Windows 提交

--- service/service-vod/src/main/java/com/atliyue/service/VodService.java
+++
@@ -1,8 +0,0 @@
-package com.atliyue.service;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface VodService {
-    String getVideoId(MultipartFile file);
-    String deleteVideoById(String videoId);
-}
