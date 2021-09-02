a3412abfe26192525d9e6a579d80d670bc2e3dcd
liyue <602100452@qq.com>
2021-09-02 23:06:25 +0800

    mac提交

--- service/service-edu/src/main/java/com/atliyue/edu/fegin/EduVodClient.java
+++ service/service-edu/src/main/java/com/atliyue/edu/fegin/EduVodClient.java
@@ -2,11 +2,12 @@ package com.atliyue.edu.fegin;
 
 import com.liyue.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+@Component
 @FeignClient(name = "edu-vod",fallback = VodCallback.class)
 public interface EduVodClient {
     @DeleteMapping("delete/{videoId}")
-    public Result deleteVideo(@PathVariable String videoId);
+    public Result deleteVideo(@PathVariable("videoId") String videoId);
 }
