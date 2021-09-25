4d55e355fcd5598bc4fa348999032bbf0fca8458
liyue <602100452@qq.com>
2021-09-25 16:21:59 +0800

    mac提交

---
+++ service/service-msm/src/main/java/com/liyue/msmApplication.java
@@ -0,0 +1,11 @@
+package com.liyue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class msmApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(msmApplication.class);
+    }
+}
