5f936c91d6ccd8b08c5bd2245bb51e419b1974cc
liyue <602100452@qq.com>
2021-09-27 19:45:16 +0800

    mac提交

--- service/service-ucenter/src/main/java/com/atliyue/ucenter/controller/MemberController.java
+++ service/service-ucenter/src/main/java/com/atliyue/ucenter/controller/MemberController.java
@@ -1,12 +1,16 @@
 package com.atliyue.ucenter.controller;
 
 
+import com.atliyue.ucenter.entity.Member;
 import com.atliyue.ucenter.service.MemberService;
 import com.atliyue.ucenter.vo.Ucenter;
 import com.liyue.result.Result;
+import com.liyue.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -39,11 +43,15 @@ public class MemberController {
     @PostMapping("register")
     public Result register(@RequestBody Ucenter ucenter){
         memberService.register(ucenter);
-
         return Result.ok();
     }
-
-
+    //获取token中的参数值；
+    @GetMapping("getToken")
+    public Result getEntityOfToken(HttpServletRequest httpServletRequest){
+        String memberIdByJwtToken = JwtUtil.getMemberIdByJwtToken(httpServletRequest);
+        Member member = memberService.getById(memberIdByJwtToken);
+        return Result.ok().data("member",member);
+    }
 
 }
 
