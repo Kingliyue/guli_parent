170482b0eb013971ea705d175444dbb6e4f92484
Kingliyue <602100452@qq.com>
2021-09-28 00:00:01 +0800

    Windows 提交

--- service/service-msm/src/main/java/com/liyue/controller/MsmController.java
+++ service/service-msm/src/main/java/com/liyue/controller/MsmController.java
@@ -4,15 +4,13 @@ import com.liyue.result.Result;
 import com.liyue.service.MsmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("msm")
+@CrossOrigin
 public class MsmController {
     @Autowired
     private RedisTemplate redisTemplate;
